e1ca52dac372f57400e0351ba2c252dc15fe9651
kristina <kristina@kristina.kristina>
2022-10-20 18:30:16 +0700

    Markup of main blocks

--- frontend/src/Room.js
+++ frontend/src/Room.js
@@ -4,10 +4,14 @@ import './RoomStyle.css';
 export default function Room() {
     return(
         <div>
-            <img className="imgs" src="crocodileWelcoming.png" />
-            <button className="game-creation-field" style={{fontSize: 36, fontWeight: 700}}>
+            <img className="imgw" src="crocodileWelcoming.png" />
+            <button className="game-creation-button" style={{fontSize: 36, fontWeight: 700}}>
                 Создать комнату
             </button>
+            <div className="game-creation-field">
+            </div>
+            <img className="imgm" src="crocodileMarsh.png" />
+            <div className="table"></div>
         </div>
     )
 }
