4500d62bcf0ad4c216d8f92dfe655516412701d1
kristina <kristina@kristina.kristina>
2022-10-20 20:44:26 +0700

    Added input field

--- frontend/src/roomComponent/RoomCreateComponent.js
+++ frontend/src/roomComponent/RoomCreateComponent.js
@@ -9,6 +9,7 @@ export default function RoomCreateComponent() {
                 <label className="lable-text" style={{top: 40, left: 90}}> Без пароля</label>
                 <label className="lable-text" style={{top: 92}}> Свободных мест:</label>
                 <label className="lable-text" style={{top: 144}}> Словари:</label>
+                <textarea className="number-players"> fffffff</textarea>
                 <DictionarComponent/>
             </div>
         </div>
