849509f4f5a442e9f019b1a6bbf3541a03c1f5df
kristina <kristina@kristina.kristina>
2022-10-20 22:20:36 +0700

    add multi-select-component

--- frontend/src/roomComponent/RoomCreateComponent.js
+++ frontend/src/roomComponent/RoomCreateComponent.js
@@ -6,10 +6,10 @@ export default function RoomCreateComponent() {
         <div>
             <div className="game-creation-field">
                 <input type="checkbox" className="checkbox-room"/>
-                <label className="lable-text" style={{top: 40, left: 90}}> Без пароля</label>
+                <label className="lable-text" style={{top: 40}}> Без пароля</label>
                 <label className="lable-text" style={{top: 92}}> Свободных мест:</label>
                 <label className="lable-text" style={{top: 144}}> Словари:</label>
-                <textarea className="number-players"> fffffff</textarea>
+                <textarea className="number-players"> </textarea>
                 <DictionarComponent/>
             </div>
         </div>
