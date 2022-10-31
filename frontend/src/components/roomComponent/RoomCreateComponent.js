f8b11c1212aff063887b25274d67ed3b059e31f7
kristina <k.galisheva@g.nsu.ru>
2022-10-31 13:23:42 +0700

    Checked that entered the number in the field

--- frontend/src/components/roomComponent/RoomCreateComponent.js
+++ frontend/src/components/roomComponent/RoomCreateComponent.js
@@ -2,12 +2,28 @@ import React from "react";
 import '../../styles/RoomStyle.css';
 import DictionarComponent from "./DictionarComponent";
 
-function checkMessage() {
-    document.getElementById('number').style.color='red'
-
-}
 
 const RoomCreateComponent = () => {
+    const[message, setMessage] = React.useState('');
+
+    const newMessage = (e) => {
+        let a = e.target.value;
+        setMessage(a);
+        if (message == ''){
+            document.getElementById('CountPlayers').style.color = "black";
+        }
+    }
+
+    const checkMessage = () => {
+        if (!isNaN(parseFloat(message)) && !isNaN(message - 0) ){
+            document.getElementById('CountPlayers').style.color = "green";
+        }
+        else {
+            document.getElementById('CountPlayers').style.color = "red";
+        }
+    }
+
+
     return (
 
         <div>
@@ -19,7 +35,7 @@ const RoomCreateComponent = () => {
                 <label className="lable-text1"> Без пароля</label>
                 <label className="lable-text2"> Свободных мест:</label>
                 <label className="lable-text3"> Словари:</label>
-                <input className="number-players" type="text" id="number"/>
+                <input className="number-players" type="text" id="CountPlayers" onChange={newMessage}/>
                 <DictionarComponent/>
             </div>
         </div>
