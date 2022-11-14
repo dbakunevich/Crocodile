5cff3fa86893083a803e55e8f1ad7b2cd1c5a554
alex <aleksdorn69@gmail.ru>
2022-11-14 16:03:04 +0700

    функциональная рисовалка без бека без красоты

--- frontend/src/components/playerComponent/Drawer.js
+++ frontend/src/components/playerComponent/Drawer.js
@@ -1,23 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import CommunityPart from "./CommunityPart";
+import Canvas from "./Canvas"
 import "../../styles/Player.css"
 
+
 const Drawer = () => {
+
+    const [color, setColor] = useState("black");
+    const [width, setWidth] = useState(5);
+
+    const changeColor = (color) => {
+        setColor(color);
+    }
+    const changeWidth = (width) => {
+        setWidth(width);
+    }
+
+    const chC = (e) => {
+        changeColor(e.target.value);
+    }
+
+    const chW = (e) => {
+        console.log(e.target.value);
+        changeWidth(e.target.value);
+    }
+
+
+
     return (
         <div className="parent-container">
-            <div className="draw-board" >drawing field</div>
+            <div className="draw-board" >
+                <Canvas
+                    CanWidth={1200}
+                    CanHeight={720}
+                    color={color}
+                    width={width}
+                />
+            </div>
             <div className="communication-part">
                 <div className="settings">settings</div>
                 <CommunityPart/>
             </div>
             <div className="drawing-things">
-                <img className={"colorImage"} src={"gameField/yellow.png"} />
-                <img className={"colorImage"} src={"gameField/red.png"} />
-                <img className={"colorImage"} src={"gameField/black.png"} />
-                <img className={"colorImage"} src={"gameField/green.png"} />
-                <img className={"colorImage"} src={"gameField/blue.png"} />
-                <img className={"colorImage"} src={"gameField/brown.png"} />
-
+                <img className={"colorImage"} src={"gameField/yellow.png"} onClick={()=>{changeColor("yellow")}} alt={"no"} />
+                <img className={"colorImage"} src={"gameField/red.png"} onClick={()=>{changeColor("red")}} alt={"no"}/>
+                <img className={"colorImage"} src={"gameField/black.png"} onClick={()=>{changeColor("black")}} alt={"no"}/>
+                <img className={"colorImage"} src={"gameField/green.png"} onClick={()=>{changeColor("green")}}  alt={"no"}/>
+                <img className={"colorImage"} src={"gameField/blue.png"} onClick={()=>{changeColor("blue")}} alt={"no"}/>
+                <img className={"colorImage"} src={"gameField/brown.png"} onClick={()=>{changeColor("brown")}} alt={"no"}/>
+                <img className={"colorImage"} src={"gameField/rubber.png"} onClick={()=>{changeColor("white");}} alt={"no"}/>
+                <input type="color"  onChange={chC}></input>
+                <input type="range" min={1} max={20} onChange={chW}></input>
             </div>
         </div>
     )
