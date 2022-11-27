dd3efbc7d08731d9aae86e38fc75222faf1d3038
kristina <k.galisheva@g.nsu.ru>
2022-11-27 18:44:09 +0700

    displaying password

--- frontend/src/pages/Authorization.js
+++ frontend/src/pages/Authorization.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 import '../styles/Authorization.css';
+
 class Authorization extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hidden: true,
+            password: '',
+        };
+
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.toggleShow = this.toggleShow.bind(this);
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
+
+    toggleShow() {
+        this.setState({ hidden: !this.state.hidden });
+    }
+
+    componentDidMount() {
+        if (this.props.password) {
+            this.setState({ password: this.props.password });
+        }
+    }
+
     render() {
         return (
             <div className="autho">
+                <label id="hiy"></label>
                 <label className="login-lable"> Логин:</label>
                 <input className="login" type="text"/>
                 <label className="pass-lable"> Пароль:</label>
-                <input className="pass" type="text"/>
+                <input className="pass"
+                       type={this.state.hidden ? 'password' : 'text'}
+                       value={this.state.password}
+                       onChange={this.handlePasswordChange}></input>
+                <button className="eye-button" onClick={this.toggleShow}>
+                    <img className="eye" alt="no foto" src="eye.png"/>
+                </button>
                 <button className="in-button"> Войти </button>
                 <a href='/Recovery' className="forgot-password" > Забыли пароль?</a>
                 <label className="no-account"> Нет аккаунта?</label>
--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -1,6 +1,48 @@
 import React, {Component} from 'react';
 import '../styles/Registration.css';
 class Registration extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hiddenfirst: true,
+            passwordfirst: '',
+            hiddensecond: true,
+            passwordsecnd: '',
+        };
+
+        this.handlePasswordChangeFirst = this.handlePasswordChangeFirst.bind(this);
+        this.toggleShowFirst = this.toggleShowFirst.bind(this);
+        this.handlePasswordChangeSecond = this.handlePasswordChangeSecond.bind(this);
+        this.toggleShowSecond = this.toggleShowSecond.bind(this);
+    }
+
+    handlePasswordChangeFirst(e) {
+        this.setState({ passwordfirst: e.target.value });
+    }
+
+    toggleShowFirst() {
+        this.setState({ hiddenfirst: !this.state.hiddenfirst });
+    }
+
+    handlePasswordChangeSecond(e) {
+        this.setState({ passwordsecond: e.target.value });
+    }
+
+    toggleShowSecond() {
+        this.setState({ hiddensecond: !this.state.hiddensecond });
+    }
+
+    componentDidMount() {
+        if (this.props.passwordfirst) {
+            this.setState({ passwordfirst: this.props.passwordfirst });
+        }
+        if (this.props.passwordsecond) {
+            this.setState({ passwordsecond: this.props.passwordsecond });
+        }
+    }
+
     render() {
         return (
             <div className="regist">
@@ -9,9 +51,21 @@ class Registration extends Component {
                 <label className="post-lable"> Почта:</label>
                 <input className="post" type="text"/>
                 <label className="password-lable"> Пароль:</label>
-                <input className="password" type="text"/>
+                <input className="password"
+                       type={this.state.hiddenfirst ? 'password' : 'text'}
+                       value={this.state.passwordfirst}
+                       onChange={this.handlePasswordChangeFirst}/>
+                <button className="eye-button" onClick={this.toggleShowFirst}>
+                    <img className="eye" alt="no foto" src="eye.png"/>
+                </button>
                 <label className="passworddb-lable"> Повторите пароль:</label>
-                <input className="passworddb" type="text"/>
+                <input className="passworddb"
+                       type={this.state.hiddensecond ? 'password' : 'text'}
+                       value={this.state.passwordsecond}
+                       onChange={this.handlePasswordChangeSecond}/>
+                <button className="eyedb-button" onClick={this.toggleShowSecond}>
+                    <img className="eye" alt="no foto" src="eye.png"/>
+                </button>
                 <button className="reg-button"> Зарегистрироваться </button>
                 <a className="authorization" href='/Authorization' > Уже есть аккаунт</a>
             </div>
